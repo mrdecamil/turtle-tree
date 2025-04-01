662867abc1042f9d7d0ae9f2536fac9c6098ab4e
mrdecamil <danielcruzacono@gmail.com>
2025-04-01 11:24:46 -0600

    tortuga.py

---
+++ import turtle.py
@@ -0,0 +1,31 @@
+import turtle
+import random
+
+def koch(t, order, size):
+  if order == 0:
+    t.forward(size)
+  else:
+    koch(t, order-1, size/3)
+    t.left(90)
+    koch(t, order-1, size/3)
+    t.right(120)
+    koch(t, order-1, size/3)
+    t.left(90)
+    koch(t, order-1, size/3)
+
+
+window = turtle.Screen()
+window.bgcolor("white")
+
+colors = ("blue", "red", "white", "purple")
+t = turtle.Turtle()
+t.speed(0)
+t.color("color string: %s" % str(colors)")
+
+t.penup()
+t.goto(-150, 90)
+t.pendown()
+
+for i in range(6):
+  koch(t, 4, 300)
+  t.right(120)
